4bb20cf48890f3aee98604482afb103f7ec423d3
Christian Bertram <cbertramdk@gmail.com>
2016-02-11 16:42:04 +0000

    Works on server

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 
-app.listen(1234);
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+var io = require('socket.io').listen(server);;
+
+server.listen(7777);
+
 var people = {};
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-  io.sockets.on('connection', function (client) {
+io.sockets.on('connection', function (client) {
   client.on('join', function(name) {
-	people[client.id] = name;
-	client.emit('update', "You have connected to the server.");
-	io.sockets.emit('update', name + " has joined the server.");
+  	people[client.id] = name;
+  	client.emit('update', "You have connected to the server.");
+  	io.sockets.emit('update', name + " has joined the server.");
 
-  function updatePeople(people) {
-    var peopleString = "";
-    for (var id in people) {
-      peopleString += people[id] + ", ";
+    function updatePeople(people) {
+      var peopleString = "";
+      for (var id in people) {
+        peopleString += people[id] + ", ";
+      }
+      io.sockets.emit('update-people', peopleString);
     }
-    io.sockets.emit('update-people', peopleString);
-  }
-  updatePeople(people);
-
-	client.on('send', function(msg) {
-		io.sockets.emit('chat', people[client.id], msg);
-	});
-
-  client.on('disconnect', function() {
-    io.sockets.emit('update', people[client.id] + "has left the server.");
-    delete people[client.id];
     updatePeople(people);
-  });
+
+  	client.on('send', function(msg) {
+  		io.sockets.emit('chat', people[client.id], msg);
+  	});
+
+    client.on('disconnect', function() {
+      io.sockets.emit('update', people[client.id] + "has left the server.");
+      delete people[client.id];
+      updatePeople(people);
+    });
   });
 });
